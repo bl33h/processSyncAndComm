46568601515fb338e7fde0feb76d39928a45ab2b
bl33h <per21371@uvg.edu.gt>
2024-02-06 22:37:46 -0600

    task 3 part 2

--- parent.c
+++ parent.c
@@ -1,29 +1,30 @@
 /*---------------------------------------------------------------------------
 Copyright (C), 2024-2025, Sara Echeverria (bl33h)
 @author Sara Echeverria
-FileName: time1.c
+FileName: parent.c
 @version: I
 Creation: 06/02/2024
 Last modification: 06/02/2024
 ------------------------------------------------------------------------------*/
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 
 int main() {
     pid_t pid = fork();
 
     if (pid == -1) {
-        // error while forking
-        perror("Error while forking");
+        perror("Error in fork");
         return 1;
     } else if (pid == 0) {
-        // Ccild process
-        printf("This is the child process.\n");
+        // child process
+        for (int i = 1; i <= 4000000; ++i) {
+            printf("%d\n", i);
+        }
     } else {
         // parent process
-        while(1) {
-
+        while (1) {
         }
     }
 
